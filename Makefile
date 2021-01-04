be1ffa52a1862eb08c6867525b608421cd4b203a
mel-kada <mel-kada@student.42lyon.fr>
2021-01-04 17:33:25 +0100

    ok

--- Makefile
+++ Makefile
@@ -11,20 +11,21 @@ SRCS = ./srcs/ft_strlen.s\
        ./srcs/ft_strcmp.s\
        ./srcs/ft_strdup.s\
        ./srcs/ft_write.s\
-	   ./srcs/ft_read.s
+	./srcs/ft_read.s
 
 OBJS = $(SRCS:.s=.o)
 
 all:		$(NAME)
 
 $(NAME):	$(OBJS)
-			ar rcs $(NAME) $(OBJS)
+			@ar rcs $(NAME) $(OBJS)
+			@echo "----- Finished -----"
 %.o:		%.s
-			$(NASM) $(NASMFLAGS) $< -o $@
+			@$(NASM) $(NASMFLAGS) $< -o $@
 clean:
-		rm -rf $(OBJS)
+		@rm -rf $(OBJS)
 fclean:		clean
-		rm -rf $(NAME) a.out
+		@rm -rf $(NAME) a.out
 re:		fclean all
 
 .PHONY:		all clean fclean re
--- srcs/libasm.h
+++ srcs/libasm.h
@@ -1,8 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-kada <mel-kada@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/04 17:01:56 by mel-kada          #+#    #+#             */
+/*   Updated: 2021/01/04 17:01:58 by mel-kada         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBASM_H
 # define LIBASM_H
 
-int	ft_strlen(const char *str);
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int		ft_strlen(const char *str);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, const char *src);
-int	ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
 
 #endif
